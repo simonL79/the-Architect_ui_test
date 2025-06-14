df09d9a8d1e88020ddf950f263459dc0504d6b00
simonL79 <simonlindsay7988@gmail.com>
2025-06-14 15:38:47 +0100

    Add components/Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className='p-5 bg-blue-500 text-white'>
+      <p>&copy; 2022 Online Food Ordering</p>
+    </footer>
+  );
+}
