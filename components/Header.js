092f87bb24c48c28c7dbb589de366b2d2de75da2
simonL79 <simonlindsay7988@gmail.com>
2025-06-14 15:38:47 +0100

    Add components/Header.js

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className='p-5 bg-blue-500 text-white'>
+      <h1 className='text-2xl'>Online Food Ordering</h1>
+    </header>
+  );
+}
